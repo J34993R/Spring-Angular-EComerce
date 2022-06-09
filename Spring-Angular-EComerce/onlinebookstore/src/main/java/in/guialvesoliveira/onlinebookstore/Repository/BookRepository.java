6663ae6287b09e74b2d2521d295ce20ca2445e15
J34993R <guialves.oliveira1234@gmail.com>
2022-06-09 17:52:38 -0300

    basic route module

--- Spring-Angular-EComerce/onlinebookstore/src/main/java/in/guialvesoliveira/onlinebookstore/Repository/BookRepository.java
+++ Spring-Angular-EComerce/onlinebookstore/src/main/java/in/guialvesoliveira/onlinebookstore/Repository/BookRepository.java
@@ -1,10 +1,18 @@
 package in.guialvesoliveira.onlinebookstore.Repository;
 
 import in.guialvesoliveira.onlinebookstore.Entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin("http://localhost:4200/")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    @RestResource(path = "categoryid")
+    Page<Book> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
